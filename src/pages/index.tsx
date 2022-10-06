f5edac7cdaef60b63582f0eceb6939fe05aeb892
Javier Garcia Magaldi <javier.garcia@incentro.com>
2022-10-06 18:31:13 +0200

    add coments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import MyOrderHandler from '../components/Orden';
 
 export default function Home() {
+  //creating order
   const orderHandler: MyOrderHandler = new MyOrderHandler();
+  //adding products and quantity
   orderHandler.add(12, 4);
   orderHandler.add(21, 2);
+  //getting total amount
   const total = orderHandler.getTotal();
-  console.log('total: ', total); // 16.00€
+  console.log('total: ', total, '€'); // 16.00€
   return (
-    <div>
+    <div className='container'>
       <p>HELLO! Look the console (f12)</p>
-      <p>total price: {total}</p>
+      <p>I have added 4 units of product 12 and 2 units of product 21</p>
+      <p>total price: {total}€</p>
     </div>
   );
 }
